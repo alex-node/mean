3590554d21a0adad9f67eb2f767b2becf611e6d7
docker <docker@zergut2012.com>
2014-11-20 17:40:25 -0800

    fix unable to connecto to mongo - REF: https://github.com/meanjs/mean/issues/142

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/mean-dev',
+	db: 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
 	app: {
 		title: 'MEAN.JS - Development Environment'
 	},
